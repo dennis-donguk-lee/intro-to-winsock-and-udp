3891b6f6a7d569700cc7ee22b12972285706e0a7
Dennis Lee <dennis.donguk.lee@gmail.com>
2018-10-10 14:05:58 -0700

    Create address.

--- Main.cpp
+++ Main.cpp
@@ -19,3 +19,26 @@ SOCKET CreateSocket(const int protocol)
   return sock;
 }
 
+sockaddr_in* CreateAddress(char* ip, const int port)
+{
+  // Allocate memory for the sockaddr in structure.
+  const auto addr = new sockaddr_in;
+  addr->sin_family = AF_INET;
+  // Remember to go from host byte order to network byte order.
+  addr->sin_port = htons(port);
+
+  if (ip == nullptr)
+  {
+    // Bind to all IP addresses local machine may have.
+    addr->sin_addr.S_un.S_addr = INADDR_ANY;
+  }
+  else
+  {
+    // Take dotted_decimal string and turn it into a 32-bit IP address,
+    // in network byte order. Return INADDR_NONE on fail.
+    inet_pton(addr->sin_family, ip, &(addr->sin_addr));
+  }
+
+  return addr;
+  // Caller will be responsible for free().
+}
