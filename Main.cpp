e3f19befafb5ed03ba8dbbb12132fb474fffe739
Dennis Lee <dennis.donguk.lee@gmail.com>
2018-10-11 09:36:37 -0700

    Add send function.

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+#include <experimental/filesystem>
 
 SOCKET CreateSocket(const int protocol)
 {
@@ -34,11 +35,35 @@ sockaddr_in* CreateAddress(char* ip, const int port)
   }
   else
   {
-    // Take dotted_decimal string and turn it into a 32-bit IP address,
+    // Take dotted-decimal string and turn it into a 32-bit IP address,
     // in network byte order. Return INADDR_NONE on fail.
     inet_pton(addr->sin_family, ip, &(addr->sin_addr));
   }
 
   return addr;
   // Caller will be responsible for free().
+}
+
+int Send(const SOCKET sock, char* buf, const int len, sockaddr_in* dest)
+{
+  const auto sizeSent = sendto(sock, buf, len, 0, reinterpret_cast<sockaddr*>(dest)
+    , sizeof(sockaddr_in));
+
+  if (sizeSent == SOCKET_ERROR)
+  {
+    return -1;
+  }
+
+  return sizeSent;
+}
+
+int Receive(SOCKET sock, char* buf, int maxSize)
+{
+  return 0;
+}
+
+
+int main()
+{
+  return 0;
 }
